61aaa0387dabe49d726f691ced3ca1257edc43dc
Inku_Xuan <inkuxuan@gmail.com>
2019-05-05 23:18:11 +0800

    initial commit

---
+++ modes.js
@@ -0,0 +1,32 @@
+modes_easy = [
+    ["Ionian", "1 2 3 4 5 6 7"],
+    ["Dorian", "1 2 b3 4 5 6 b7"],
+    ["Phrygian", "1 b2 b3 4 5 b6 b7"],
+    ["Lydian", "1 2 3 #4 5 6 7"],
+    ["Mixolydian", "1 2 3 4 5 6 b7"],
+    ["Aeolian(Iorian)", "1 2 b3 4 5 b6 b7"],
+    ["Locrian", "1 b2 b3 4 b5 b6 b7"],
+];
+modes_normal = [
+    ["Melody minor", "1 2 b3 4 5 6 7"],
+    ["Melody minor Dorian", "1 b2 b3 4 5 6 b7"],
+    ["Melody minor Phrygian", "1 2 3 #4 #5 6 7"],
+    ["Phrygian major", "1 b2 3 4 5 b6 b7"],
+    ["Lydian b7", "1 2 3 #4 5 6 b7"],
+    ["Mixolydian b6", "1 2 3 4 5 b6 b7"],
+    ["Melody minor Aeolian", "1 2 b3 4 b5 b6 b7"],
+    ["Locrian(alter)", "1 b2 #2 3 #4 b6 b7"],
+    ["Locrian(alter)", "1 b2 #2 3 b5 #5 b7"],
+    ["Locrian(alter)", "1 b2 b3 b4 b5 b6 b7"],
+];
+modes_hard = [
+    ["Major Pentatonic", "1 2 3 5 6"],
+    ["Minor Pentatonic", "1 b3 4 5 b7"],
+    ["Blues", "1 b3 4 #4 5 b7"],
+    ["Hamonic major", "1 2 3 4 5 b6 7"],
+    ["Hamonic minor", "1 2 b3 4 5 b6 7"],
+    ["Whole tone", "1 2 3 #4 #5 b7"],
+    ["Chromatic", "1 b2 2 b3 3 4 #4 5 b6 6 b7 7"],
+    ["diminished", "1 2 b3 4 b5 b6 6 7"],
+    ["half-whole dminished", "1 b2 b3 3 #4 5 6 b7"],
+];
